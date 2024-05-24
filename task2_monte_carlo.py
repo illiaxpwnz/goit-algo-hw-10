5e7266a22dafbdbf0aed8b4823839f0e595f2058
Illia <79321689+illiaxpwnz@users.noreply.github.com>
2024-05-24 16:43:54 +0300

    Update task2_monte_carlo.py

--- task2_monte_carlo.py
+++ task2_monte_carlo.py
@@ -1,16 +1,46 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import scipy.integrate as spi
 
+# Визначення функції та межі інтегрування
 def f(x):
     return x ** 2
 
-a = 0
-b = 2
+a = 0  # Нижня межа
+b = 2  # Верхня межа
 
+x = np.linspace(-0.5, 2.5, 400)
+y = f(x)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, y, 'r', linewidth=2)
+
+ix = np.linspace(a, b)
+iy = f(ix)
+ax.fill_between(ix, iy, color='gray', alpha=0.3)
+
+ax.set_xlim([x[0], x[-1]])
+ax.set_ylim([0, max(y) + 0.1])
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)')
+
+ax.axvline(x=a, color='gray', linestyle='--')
+ax.axvline(x=b, color='gray', linestyle='--')
+ax.set_title('Графік інтегрування f(x) = x^2 від ' + str(a) + ' до ' + str(b))
+plt.grid()
+plt.show()
+
+# Обчислення інтегралу методом Монте-Карло
 N = 100000
-
 x_random = np.random.uniform(a, b, N)
 y_random = f(x_random)
-
 integral_mc = (b - a) * np.mean(y_random)
 
 print(f"Інтеграл (метод Монте-Карло): {integral_mc}")
+
+# Обчислення інтегралу за допомогою функції quad
+result, error = spi.quad(f, a, b)
+
+print(f"Інтеграл (функція quad): {result}")
+print(f"Інтеграл (аналітичне значення): {(b ** 3) / 3 - (a ** 3) / 3}")
